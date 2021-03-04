80458edbb137765169d0e270a5adcc4ce80b4d86
unknown <alfredo_stochiero@hotmail.com>
2021-03-04 17:06:10 -0300

    busca por nome,marca,condição e categoria funcionando

--- product/views.py
+++ product/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Product,ProductImages, Category
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -11,20 +14,33 @@ def product_list(request, category_slug=None):
     categorylist = Category.objects.annotate(total_products=Count('product'))
 
     if category_slug :
-        category = Category.objects.get(slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         productlist = productlist.filter(category=category)
+
+    search_query = request.GET.get('q')
+    # se for feito uma busca, essa irá filtrar se existe algum produto,descrição,marca, ou condição  do produto com essa palavra
+    if search_query :
+        productlist = productlist.filter(
+            Q(name__icontains = search_query) |
+            Q(description__icontains = search_query) |
+            Q(brand__brand_name__icontains = search_query) |
+            Q(condition__icontains= search_query) |
+            Q(category__category_name__icontains = search_query)
+        )
+
+
 
     paginator = Paginator(productlist, 2)
     page = request.GET.get('page')
     productlist = paginator.get_page(page)
 
-    context = {'productlist': productlist, 'categorylist': categorylist}
+    context = {'productlist': productlist, 'categorylist': categorylist, 'category': category}
     return render(request, 'product/product_list.html', context)
 
 
 def product_detail(request, product_slug):
 
-    product = Product.objects.get(slug=product_slug)
+    product = get_object_or_404(Product, slug=product_slug)
 
     product_images = ProductImages.objects.filter(product=product)
     context = {'product': product, 'product_images': product_images}
