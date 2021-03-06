08d1a963c1f8abcdc51b69ca96754f0d8633da75
unknown <alfredo_stochiero@hotmail.com>
2021-03-06 19:37:18 -0300

    'teste'

--- OlxBetim/settings.py
+++ OlxBetim/settings.py
@@ -145,23 +145,23 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 '''
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = str(BASE_DIR / 'media/')
-
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
-# arquivos de media :
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_ROOT = str(BASE_DIR / 'media')
+MEDIA_ROOT = str(BASE_DIR / 'media/')
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# #STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+# # arquivos de media :
+# MEDIA_URL = '/media/'
+# #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_ROOT = str(BASE_DIR / 'media')
 
 
 
